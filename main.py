7f5245a3afe70739d57c809b5b5d190a3768e100
Jonas MPN <you@example.com>
2022-08-29 12:04:15 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+class Test:
+    def __int__(self):
+        pass
+
+    def task_one(self, param1, param2, param3):
+        return param1+param2+param3
+
