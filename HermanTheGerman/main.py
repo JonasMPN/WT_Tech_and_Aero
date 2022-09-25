1a60737ef82f40c1d4e330ea87b4dbf6edcf8a78
Jonas <jonas.mpn@gmail.com>
2022-09-25 20:04:54 +0200

    Updated to new class format

--- HermanTheGerman/main.py
+++ HermanTheGerman/main.py
@@ -1,10 +1,19 @@
 from BEM import BEM
+import numpy as np
+import matplotlib.pyplot as plt
 
-test = BEM()
-test.set_constants(8, 2.61, 31, 3, 1.225)
-c0 = 0.5
-c1 = 1
-c2 = 1.5
-print(f"Test results with c={c0}m: \n {test.solve(24.5, c0, 0.5, 0.01)}")
-print(f"Test results with c={c1}m: \n {test.solve(24.5, c1, 0.5, 0.01)}")
-print(f"Test results with c={c2}m: \n {test.solve(24.5, c2, 0.5, 0.01)}")
+rpm = 9.6
+bem = BEM("airfoil_data",
+          "blade_data.txt",
+          "combined_data.txt",
+          {"c_l": ["rel_thickness", "alpha"], "c_d": ["rel_thickness", "alpha"]})
+bem.set_constants(v0=11.4,
+                  omega=rpm/30*np.pi,
+                  rotor_radius=89.17,
+                  n_blades=3,
+                  air_density=1.225)
+radius, N, T = bem.solve(pitch=0)
+plt.plot(radius, N)
+plt.plot(radius, T)
+plt.grid()
+plt.show()
