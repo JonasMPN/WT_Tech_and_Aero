6b0e7e180ee9132ad9f825849ac9d08053180cad
Jonas MPN <you@example.com>
2022-10-08 16:22:09 +0200

    Updated to new data_handling.py structure

--- HermanTheGerman/main.py
+++ HermanTheGerman/main.py
@@ -1,30 +1,62 @@
 from BEM import BEM
-import matplotlib.pyplot as plt
 from matplotlib import cm
 import numpy as np
 from helper_functions import Helper
+from data_handling import BemData
 helper = Helper()
-res = 50
-v_min, v_max, rpm_min, rpm_max = 10, 25, 6, 9.6
-omega_min, omega_max = rpm_min*np.pi/30, rpm_max*np.pi/30
-rotor_radius = 89.17
-bem = BEM("airfoil_data",
-          "blade_data_new.txt",
-          "combined_data_new.txt",
-          {"c_l": ["rel_thickness", "alpha"], "c_d": ["rel_thickness", "alpha"]})
-bem.set_constants(rotor_radius=rotor_radius,
-                  n_blades=3,
-                  v0=10,
-                  air_density=1.225)
-tip_speed_ratios, pitch_angles, c_Ps = bem.optimise(tpr_interval=(omega_min*rotor_radius/v_max, omega_max*rotor_radius/v_min),
-                                                    pitch_interval=(-2,5), resolution=res)
+bem_data = BemData("data")
 
-fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-ax.plot_surface(tip_speed_ratios,pitch_angles, c_Ps, cmap=cm.coolwarm, linewidth=0, antialiased=False)
-helper.handle_axis(ax, r"$c_p$", x_label="TSR", y_label="pitch", z_label="c_p")
-plt.close(helper.handle_figure(fig, f"data/surface_{res}.png"))
+do = {
+    "calculate": True,
+    "contourf": False,
+    "surface": False,
+}
 
-fig, ax = plt.subplots()
-ax.contourf(tip_speed_ratios, pitch_angles, c_Ps)
-helper.handle_axis(ax, r"$c_p$", x_label="TSR", y_label="pitch")
-plt.close(helper.handle_figure(fig, f"data/contour_{res}.png"))
+res = 500
+if do["calculate"]:
+    v_min, v_max, rpm_min, rpm_max = 5, 25, 6, 9.6
+    omega_min, omega_max = rpm_min*np.pi/30, rpm_max*np.pi/30
+    rotor_radius = 89.17
+    bem = BEM("airfoil_data",
+              "blade_data_new.txt",
+              "combined_data_new.txt",
+              {"c_l": ["rel_thickness", "alpha"], "c_d": ["rel_thickness", "alpha"]})
+    bem.set_constants(rotor_radius=rotor_radius,
+                      n_blades=3,
+                      v0=10,
+                      air_density=1.225)
+    bem_data.save(res, *bem.optimise(tpr_interval=(omega_min*rotor_radius/v_max, omega_max*rotor_radius/v_min),
+                                pitch_interval=(-2,5), resolution=res))
+
+if do["contourf"]:
+    contourf_kwargs = {
+        "levels": [0,0.1,0.2,0.3,0.4,0.44,0.45,0.46, 0.465, 0.5],
+        "cmap": cm.RdYlGn}
+    axes_kwargs = {
+        "title": r"$c_P$",
+        "x_label": "TSR",
+        "y_label": "pitch",
+        "font_size": 20}
+    figure_kwargs = {
+        "size": (18.5, 10)}
+    bem_data.contourf(resolution=res,
+                      contourf_kwargs=contourf_kwargs,
+                      axes_kwargs=axes_kwargs,
+                      figure_kwargs=figure_kwargs)
+
+if do["surface"]:
+    surface_kwargs = {
+        "linewidth": 0,
+        "cmap": cm.RdYlGn,
+        "antialiased": False}
+    axes_kwargs = {
+        "title": r"$c_P$",
+        "x_label": "TSR",
+        "y_label": "pitch",
+        "z_label": r"$c_P$"}
+    figure_kwargs = {
+        "size": (18.5, 10)}
+    bem_data.surface(resolution=res,
+                     surface_kwargs=surface_kwargs,
+                     axes_kwargs=axes_kwargs,
+                     figure_kwargs=figure_kwargs)
