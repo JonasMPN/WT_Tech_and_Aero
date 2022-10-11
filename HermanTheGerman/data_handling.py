5f71406b6004535f6f8874362bfbb921e4c07a45
Jonas <jonas.mpn@gmail.com>
2022-10-11 11:45:26 +0200

    Changed plot appearance

--- HermanTheGerman/data_handling.py
+++ HermanTheGerman/data_handling.py
@@ -61,7 +61,7 @@ class BemData:
 
         tsr = np.load(self.dir_data+f"/tsr_{resolution}.npy")
         pitch = np.load(self.dir_data+f"/pitch_{resolution}.npy")
-        cps = np.load(self.dir_data+f"/cps_{resolution}.npy")
+        cps = np.load(self.dir_data+f"/c_Ps_{resolution}.npy")
         fig, ax = plt.subplots()
         CS = ax.contour(tsr, pitch, cps, **contourf_kwargs)
         ax.clabel(CS, inline=True, fontsize=10)
@@ -70,8 +70,7 @@ class BemData:
         return None
 
     def surface(self,
-                resolution: int,
-                surface_kwargs: dict=None,
+                resolution: int,                surface_kwargs: dict=None,
                 axes_kwargs: dict=None,
                 figure_kwargs: dict=None,
                 add_to_fig_name: str="") -> None:
@@ -81,15 +80,16 @@ class BemData:
 
         tsr = np.load(self.dir_data+f"/tsr_{resolution}.npy")
         pitch = np.load(self.dir_data+f"/pitch_{resolution}.npy")
-        cps = np.load(self.dir_data+f"/cps_{resolution}.npy")
+        cps = np.load(self.dir_data+f"/c_Ps_{resolution}.npy")
         fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
         ax.plot_surface(tsr, pitch, cps, **surface_kwargs)
         helper.handle_axis(ax, **axes_kwargs)
-        plt.close(helper.handle_figure(fig, self.dir_data+f"/contour_{resolution}{add_to_fig_name}.png", **figure_kwargs))
+        plt.close(helper.handle_figure(fig, self.dir_data+f"/surface_{resolution}{add_to_fig_name}.png",
+                                       **figure_kwargs))
         return None
 
     def pitch_curve(self,
-                    pitch_step_size: float,
+                    init_pitch_step_size: float,
                     axes_kwargs: dict=None,
                     figure_kwargs: dict=None,
                     add_to_fig_name: str="") -> None:
@@ -104,60 +104,87 @@ class BemData:
         axes_kwargs = {} if axes_kwargs is None else axes_kwargs
         figure_kwargs = {} if figure_kwargs is None else figure_kwargs
 
-        df_ramp = pd.read_csv(self.dir_data+f"/{pitch_step_size}_ramp.dat", index_col=None)
-        df_control = pd.read_csv(self.dir_data+f"/{pitch_step_size}_control.dat", index_col=None)
+        df_ramp = pd.read_csv(self.dir_data+f"/{init_pitch_step_size}_ramp.dat", index_col=None)
+        df_control = pd.read_csv(self.dir_data+f"/{init_pitch_step_size}_control.dat", index_col=None)
+
+        v0 = df_ramp["v0"].tolist()+df_control["v0"].tolist()
+        df_ramp["power"] = df_ramp["power"]/1e7
+        df_ramp["thrust"] = df_ramp["thrust"]/1e6
+
+        df_control["power_feather"] = df_control["power_feather"]/1e7
+        df_control["power_stall"] = df_control["power_stall"]/1e7
+        df_control["thrust_stall"] = df_control["thrust_stall"]/1e6
+        df_control["thrust_feather"] = df_control["thrust_feather"]/1e6
+
 
         fig, ax = plt.subplots()
-        ax.plot(df_ramp["v0"], df_ramp["power"], "k", label="ramp up")
-        ax.plot(df_control["v0"], df_control["power_stall"], label="stall")
-        ax.plot(df_control["v0"], df_control["power_feather"], label="feather")
-        helper.handle_axis(ax, title="Power curve", legend=True, x_label=r"$v_0$ in $\frac{m}{s}$", y_label="power in W",
-                           font_size=20)
-        plt.close(helper.handle_figure(fig, self.dir_data+f"/power_{pitch_step_size}.png"))
-
-        fig, ax = plt.subplots()
-        ax.plot(df_ramp["v0"], df_ramp["pitch"], "k", label="ramp up")
-        ax.plot(df_control["v0"], df_control["pitch_stall"].apply(np.rad2deg), label="stall")
-        ax.plot(df_control["v0"], df_control["pitch_feather"].apply(np.rad2deg), label="feather")
+        control_v0 = [df_ramp["v0"].tolist()[-1]] + df_control["v0"].tolist()
+        pitch_stall = [df_ramp["pitch"].apply(np.rad2deg).tolist()[-1]]+df_control["pitch_stall"].apply(
+            np.rad2deg).tolist()
+        feather_stall = [df_ramp["pitch"].apply(np.rad2deg).tolist()[-1]]+df_control["pitch_feather"].apply(
+            np.rad2deg).tolist()
+        ax.plot(df_ramp["v0"], df_ramp["pitch"].apply(np.rad2deg), "k", label="below rated", lw=3)
+        ax.plot(control_v0,pitch_stall, label="stall", lw=3)
+        ax.plot(control_v0, feather_stall, label="feather", lw=3)
         helper.handle_axis(ax, title="Pitch curve", legend=True, x_label=r"$v_0$ in $\frac{m}{s}$", y_label="pitch in °",
-                           font_size=20)
-        plt.close(helper.handle_figure(fig, self.dir_data+f"/pitch_{pitch_step_size}.png"))
+                           **axes_kwargs)
+        plt.close(helper.handle_figure(fig, self.dir_data+f"/pitch_{init_pitch_step_size}.png"))
 
         fig, ax = plt.subplots()
-        ax.plot(df_control["v0"], df_control["stall_pitch_steps"], label="stall")
-        ax.plot(df_control["v0"], df_control["feather_pitch_steps"], label="feather")
+        ax.plot(v0, (df_ramp["power"].tolist()+df_control["power_stall"].tolist()), "k", label="P", lw=3)
+        ax2 = ax.twinx()
+        ax2.plot(v0, df_ramp["cP"].tolist()+df_control["stall_cP"].tolist(), label=r"$c_P$", lw=3, color="r")
+        helper.handle_axis([ax, ax2], title="Power (stall control)", legend=True,
+                           x_label=r"$v_0$ in $\frac{m}{s}$", y_label=["P in 10MW", r"$c_P$"], legend_loc=6,
+                           **axes_kwargs)
+        plt.close(helper.handle_figure(fig, self.dir_data+f"/power_stall_{init_pitch_step_size}.png"))
+
+        fig, ax = plt.subplots()
+        ax.plot(v0, (df_ramp["power"].tolist()+df_control["power_feather"].tolist()), "k", label="P", lw=3)
+        ax2 = ax.twinx()
+        ax2.plot(v0, df_ramp["cP"].tolist()+df_control["feather_cP"].tolist(), label=r"$c_P$", lw=3, color="r")
+        helper.handle_axis([ax, ax2], title="Power (feather control)", legend=True,
+                           x_label=r"$v_0$ in $\frac{m}{s}$", y_label=["P in 10MW", r"$c_P$"], legend_loc=6,
+                           **axes_kwargs)
+        plt.close(helper.handle_figure(fig, self.dir_data+f"/power_feather_{init_pitch_step_size}.png"))
+
+        fig, ax = plt.subplots()
+        ax.plot(v0, (df_ramp["thrust"].tolist()+df_control["thrust_feather"].tolist()), "k", label="T", lw=3)
+        ax2 = ax.twinx()
+        ax2.plot(v0, df_ramp["cT"].tolist()+df_control["feather_cT"].tolist(), label=r"$c_T$", lw=3, color="b")
+        helper.handle_axis([ax, ax2], title="Thrust (feather control)", legend=True,
+                           x_label=r"$v_0$ in $\frac{m}{s}$", y_label=["T in MN", r"$c_P$"], legend_loc=6,
+                           **axes_kwargs)
+        plt.close(helper.handle_figure(fig, self.dir_data+f"/thrust_feather_{init_pitch_step_size}.png"))
+
+        fig, ax = plt.subplots()
+        ax.plot(v0, (df_ramp["thrust"].tolist()+df_control["thrust_stall"].tolist()), "k", label="T", lw=3)
+        ax2 = ax.twinx()
+        ax2.plot(v0, df_ramp["cT"].tolist()+df_control["stall_cT"].tolist(), label=r"$c_T$", lw=3, color="b")
+        helper.handle_axis([ax, ax2], title="Thrust (stall control)", legend=True, legend_loc=6,
+                           x_label=r"$v_0$ in $\frac{m}{s}$", y_label=["T in MN", r"$c_P$"],
+                           **axes_kwargs)
+        plt.close(helper.handle_figure(fig, self.dir_data + f"/thrust_stall_{init_pitch_step_size}.png"))
+
+        fig, ax = plt.subplots()
+        ax.plot(df_control["v0"], df_control["stall_pitch_steps"], label="stall", lw=3)
+        ax.plot(df_control["v0"], df_control["feather_pitch_steps"], label="feather", lw=3)
         helper.handle_axis(ax, title="Pitch steps", legend=True, x_label=r"$v_0$ in $\frac{m}{s}$",
-                           y_label="pitch steps", font_size=20)
-        plt.close(helper.handle_figure(fig, self.dir_data+f"/pitch_steps_{pitch_step_size}.png"))
+                           y_label="pitch steps", **axes_kwargs)
+        plt.close(helper.handle_figure(fig, self.dir_data+f"/pitch_steps_{init_pitch_step_size}.png"))
 
         fig, ax = plt.subplots()
-        ax.plot(df_control["v0"], df_control["stall_step_size"], label="stall")
-        ax.plot(df_control["v0"], df_control["feather_step_size"], label="feather")
+        ax.plot(df_control["v0"], df_control["stall_step_size"], label="stall", lw=3)
+        ax.plot(df_control["v0"], df_control["feather_step_size"], label="feather", lw=3)
         helper.handle_axis(ax, title="Pitch step size", legend=True, x_label=r"$v_0$ in $\frac{m}{s}$",
-                           y_label="pitch step size", font_size=20)
-        plt.close(helper.handle_figure(fig, self.dir_data+f"/pitch_step_size_{pitch_step_size}.png"))
+                           y_label="pitch step size", **axes_kwargs)
+        plt.close(helper.handle_figure(fig, self.dir_data+f"/pitch_step_size_{init_pitch_step_size}.png"))
 
-        fig, ax = plt.subplots()
-        ax.plot(df_ramp["v0"], df_ramp["cP"], label="ramp")
-        ax.plot(df_control["v0"], df_control["feather_cP"], label="feather")
-        ax.plot(df_control["v0"], df_control["stall_cP"], label="stall")
-        helper.handle_axis(ax, title="Power coefficient", legend=True, x_label=r"$v_0$ in $\frac{m}{s}$",
-                           y_label=r"$c_P$", font_size=20)
-        plt.close(helper.handle_figure(fig, self.dir_data+f"/cP_{pitch_step_size}.png"))
-
-        fig, ax = plt.subplots()
-        ax.plot(df_ramp["v0"], df_ramp["cT"], label="ramp")
-        ax.plot(df_control["v0"], df_control["feather_cT"], label="feather")
-        ax.plot(df_control["v0"], df_control["stall_cT"], label="stall")
-        helper.handle_axis(ax, title="Thrust coefficient", legend=True, x_label=r"$v_0$ in $\frac{m}{s}$",
-                           y_label=r"$c_t$", font_size=20)
-        plt.close(helper.handle_figure(fig, self.dir_data+f"/cT_{pitch_step_size}.png"))
-        return None
 
     def optimum_from_resolution(self, resolution: int) -> tuple[float, float, float]:
         tsr = np.load(self.dir_data+f"/tip_speed_ratios_{resolution}.npy")
         pitch = np.load(self.dir_data+f"/pitch_angles_{resolution}.npy")
-        cps = np.load(self.dir_data+f"/c_Ps_{resolution}.npy")
+        cps = np.load(self.dir_data+f"/c_Ps__{resolution}.npy")
         id_max = np.unravel_index(cps.argmax(), (resolution, resolution))
         return np.max(cps), tsr[id_max], pitch[id_max]
 
@@ -189,35 +216,101 @@ class AshesData:
     def __init__(self, data_dir: str):
         self.dir_data = data_dir
 
-    def ascii_to_dat(self, file: str, as_type: str="dat") -> None:
+    def ascii_to_dat(self, file: str, as_type: str="dat", save: bool=True) -> None or pd.DataFrame:
+        """
+        Currently only works for these sensors:
+            - Blade [Time]
+            - Blade [Span]
+            - Rotor
+        :param file:
+        :param as_type:
+        :param save:
+        :return:
+        """
+        implemented_sensors = ["Rotor", "Blade [Span]", "Blade [Time]"]
+        information_cols ={
+            "Rotor": {"columns": 11, "data_start": 18},
+            "Blade [Span]": {"columns": 11, "data_start": 22, "be_positions": 20},
+            "Blade [Time]": {"columns": 12, "data_start": 19}
+        }
         columns = list()
         be_positions = list()
         with open(self.dir_data+f"/{file}") as data_file:
             content = data_file.readlines()
-            columns_str = content[11]
+            sensor = None
+            for sens in implemented_sensors:
+                if sens in content[0]:
+                    sensor = sens
+                    print(f"Sensor detected as {sensor}")
+                    break
+            if sensor is None:
+                raise ValueError(f"Data from {content[0]} not supported. Supported sensors are for "
+                                 f"{implemented_sensors}.")
+            columns_str = content[information_cols[sensor]["columns"]]
             content_cleaned = False
             while not content_cleaned:
-                columns.append(columns_str[:columns_str.find("[") - 1])
+                idx_bracket = columns_str.find("[")-1
+                try:
+                    idx_comma = columns_str.index(",")
+                except ValueError:
+                    idx_comma = idx_bracket
+                idx = idx_bracket if idx_bracket < idx_comma else idx_comma
+                columns.append(columns_str[:idx])
                 columns_str = columns_str[columns_str.find("\t") + 1:]
                 if "\t" not in columns_str:
                     content_cleaned = True
-            position_str = content[20]
-            got_all = False
-            while not got_all:
-                be_positions.append(float(position_str[:position_str.find(" ")]))
-                position_str = position_str[position_str.find(" ") + 1:]
-                if " " not in position_str:
-                    got_all = True
-            n_blade_elements = len(be_positions)
-            data = {column: list() for column in ["radius"]+columns}
-            for time_data in content[22:]:
-                time = float(time_data[:time_data.find("\t")])
-                time_data = time_data[time_data.find("\t")+1:]
-                data["Time"] += [time for _ in range(n_blade_elements)]
-                data["radius"] += be_positions
-                for col in columns[1:]:
-                    values = list(ast.literal_eval(time_data[:time_data.find("\t")-1].replace(" ", ",")))
-                    data[col] += values
+            spanwise = False
+            if "Blade" in sensor:
+                spanwise = True
+                position_str = content[information_cols[sensor]["be_positions"]]
+                got_all = False
+                while not got_all:
+                    be_positions.append(float(position_str[:position_str.find(" ")]))
+                    position_str = position_str[position_str.find(" ") + 1:]
+                    if " " not in position_str:
+                        got_all = True
+                n_blade_elements = len(be_positions)
+            columns = columns if "Blade" not in sensor else ["radius"]+columns
+            data = {column: list() for column in columns}
+            for time_data in content[information_cols[sensor]["data_start"]:]:
+                if spanwise:
+                    time = float(time_data[:time_data.find("\t")])
+                    time_data = time_data[time_data.find("\t")+1:]
+                    data["Time"] += [time for _ in range(n_blade_elements)]
+                    data["radius"] += be_positions
+                    for col in columns[2:]:
+                        sensor_separation = time_data.find("\t")
+                        values = list(ast.literal_eval(time_data[:sensor_separation-1].replace(" ", ",")))
+                        for i, char in enumerate(time_data[sensor_separation+1:]):
+                            if char not in [" ", "\t"]:
+                                time_data = time_data[sensor_separation+1+i:]
+                                break
+                        data[col] += values
+                else:
+                    time_data = list(ast.literal_eval(time_data.replace("\t", ",")))
+                    for col, value in zip(columns, time_data):
+                        data[col].append(value)
             df = pd.DataFrame(data)
             file_save = file[:file.find('.')]+f".{as_type}"
-            df.to_csv(self.dir_data+f"/{file_save}", index=False)
+            if save:
+                df.to_csv(self.dir_data+f"/{file_save}", index=False)
+            else:
+                return df
+
+    def extract(self,
+                file:str,
+                argument: dict):
+        """
+        :param file:
+            :param values: e.g. {"Wind speed at hub": [4,5,6,7,8,9,10]}
+        :return:
+        """
+        id_dot = file.find(".")
+        file_name = file[:id_dot]
+        file_type = file[id_dot+1:]
+        df = pd.read_csv(self.dir_data+f"/{file}", index_col=None)
+        column = [*argument][0]
+        ids = list()
+        for value in argument[column]:
+            ids.append(df.loc[df[column]==value].index.tolist()[-1])
+        df.iloc[ids, :].to_csv(self.dir_data+f"/{file_name}_extracted_{column}.{file_type}", index=False)
