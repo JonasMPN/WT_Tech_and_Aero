69cac1907589f83f914c240943773bef3b7d0e8c
Jonas MPN <you@example.com>
2022-11-26 18:42:19 +0100

    Q4 done

--- HermanTheGerman/Assignment_2/main.py
+++ HermanTheGerman/Assignment_2/main.py
@@ -1,9 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.optimize import newton
 
 do = {
     "1 and 2": False,
-    "3": True
+    "3": False,
+    "4": True
 }
 
 
@@ -14,7 +16,7 @@ if do["1 and 2"]:
         V_g = (P_mech-P_loss)/I_a
         return I_a, V_g, P_loss
 
-if do["3"]:
+if do["3"] or do["4"]:
     def refer_impedances_to_primary(transformer_ratio: float, *args):
         return [arg*transformer_ratio**2 for arg in args]
 
@@ -26,6 +28,7 @@ if do["3"]:
 
 
     def calculations(generator_power: float,
+                     reactive_power_fac: float,
                      V_grid_ref_prim: float,
                      grid_frequency: float,
                      core_inductance: complex,
@@ -37,13 +40,13 @@ if do["3"]:
         omega = grid_frequency
         Z_1 = impedance_primary
         Z_2_ref_prim = impedance_sec_ref_prim
-        power = generator_power
+        power = generator_power*(1+reactive_power_fac*1j)
 
         imp_cap = 1j*omega*C_ref_prim
         imp_core = 1j*omega*L_t
         I_sqr_fac = (Z_1+Z_2_ref_prim/(1+Z_2_ref_prim/imp_core))
         p = V_grid_ref_prim*(imp_cap*imp_sec_ref_prim+1-(imp_cap*(imp_core+imp_sec_ref_prim)+1)/(imp_core/Z_2_ref_prim+1))/I_sqr_fac
-        q = -power*(1+0.2j)/I_sqr_fac
+        q = -power/I_sqr_fac
         sqrt = np.sqrt(p**2/4-q)
         I_1 = [-p/2+sqrt, -p/2-sqrt]
         power_test = False
@@ -53,12 +56,12 @@ if do["3"]:
             I_poc_ref_prim.append(I_poc_rp)
             I_T =((imp_cap*V_grid_ref_prim+I_poc_rp)*Z_2_ref_prim+V_grid_ref_prim)/imp_core
             i_2 = I-I_T
-            V_B.append(power/I*(1+0.2j))
+            V_B.append(power/I)
             I_t.append(I_T)
             I_2.append(i_2)
             if power_test:
                 print("Supplied power by VSC-B minus power drawn from all components: ",
-                      power*(1+0.2j)-I**2*impedance_primary-I_T**2*imp_core-i_2**2*impedance_sec_ref_prim-(i_2-I_poc_rp)**2*imp_cap-I_poc_rp*V_grid_ref_prim)
+                      power-I**2*impedance_primary-I_T**2*imp_core-i_2**2*impedance_sec_ref_prim-(i_2-I_poc_rp)**2*imp_cap-I_poc_rp*V_grid_ref_prim)
         return I_poc_ref_prim, I_1, V_B, I_t, I_2
 
 
@@ -77,16 +80,32 @@ if do["3"]:
                                                                                            capacitance_cable)
     V_grid_ref_prim, = refer_voltages_to_primary(transformer_ratio, V_grid)
     imp_sec_ref_prim = 25e-3+R_cable_ref_prim+1j*(0.6e-3+ind_cable_ref_prim)
-    I_poc_ref_prim, I_1, V_B, I_t, I_2_ref_prim = calculations(generator_power= generator_power,
-                                                               V_grid_ref_prim= V_grid_ref_prim,
-                                                               grid_frequency= frequency_grid,
-                                                               core_inductance= 0.046,
-                                                               cable_capacity_ref_prim= cap_cable_ref_prim,
-                                                               impedance_primary= 25e-3+1j*0.6e-3,
-                                                               impedance_sec_ref_prim= imp_sec_ref_prim)
-    I_poc, I_2 = list(), list()
-    for i_poc_ref_prim, i_2_ref_prim in zip(I_poc_ref_prim, I_2_ref_prim):
-        i_poc, i_2 = refer_currents_to_primary(1/transformer_ratio, i_poc_ref_prim, i_2_ref_prim)
-        I_poc.append(i_poc)
-        I_2.append(i_2)
-    print(-np.asarray(I_poc)*V_grid)
+    if do["3"]:
+        I_poc_ref_prim, I_1, V_B, I_t, I_2_ref_prim = calculations(generator_power= generator_power,
+                                                                   reactive_power_fac=0.2,
+                                                                   V_grid_ref_prim= V_grid_ref_prim,
+                                                                   grid_frequency= frequency_grid,
+                                                                   core_inductance= 0.046,
+                                                                   cable_capacity_ref_prim= cap_cable_ref_prim,
+                                                                   impedance_primary= 25e-3+1j*0.6e-3,
+                                                                   impedance_sec_ref_prim= imp_sec_ref_prim)
+        I_poc, I_2 = list(), list()
+        for i_poc_ref_prim, i_2_ref_prim in zip(I_poc_ref_prim, I_2_ref_prim):
+            i_poc, i_2 = refer_currents_to_primary(1/transformer_ratio, i_poc_ref_prim, i_2_ref_prim)
+            I_poc.append(i_poc)
+            I_2.append(i_2)
+        print(-np.asarray(I_poc)*V_grid)
+    if do["4"]:
+        def residue(reactive_power_fac: float) -> float:
+            I_poc_ref_prim, _ = calculations(generator_power=generator_power, reactive_power_fac=reactive_power_fac,
+                                        V_grid_ref_prim=V_grid_ref_prim, grid_frequency=frequency_grid, core_inductance=0.046,
+                                        cable_capacity_ref_prim=cap_cable_ref_prim, impedance_primary=25e-3 + 1j * 0.6e-3,
+                                        impedance_sec_ref_prim=imp_sec_ref_prim)[0]
+            return I_poc_ref_prim.imag
+        RPF = newton(residue, x0=-0.8175)
+        I_poc_ref_prim, = calculations(generator_power=generator_power, reactive_power_fac=RPF,
+                                        V_grid_ref_prim=V_grid_ref_prim, grid_frequency=frequency_grid, core_inductance=0.046,
+                                        cable_capacity_ref_prim=cap_cable_ref_prim, impedance_primary=25e-3 + 1j * 0.6e-3,
+                                        impedance_sec_ref_prim=imp_sec_ref_prim)[0]
+        i_poc = refer_currents_to_primary(1/transformer_ratio, I_poc_ref_prim)
+        print("Reactive power coefficient: ", RPF, "yields I_poc ", i_poc)
